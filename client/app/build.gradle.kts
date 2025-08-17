6302391cde29722c5b634dbf2e91a16cbb3626eb
Ipsita Nayek <150594582+IpsitaZ@users.noreply.github.com>
2025-08-17 14:24:28 +0530

    gradle changes

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.zeniba"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.zeniba"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
